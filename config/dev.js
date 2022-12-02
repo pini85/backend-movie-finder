c28c395233c01dec6685b6eb83b70cb10ffbb1bc
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2022-12-02 13:47:19 +0200

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
   googleClientID:
-    "904726263959-7rmiu0q2vftus1q9iodns9ihlh2q0222.apps.googleusercontent.com",
-  googleClientSecret: "QSy7gFSN7kkpKxxtbZWDdE9I",
+    '904726263959-7rmiu0q2vftus1q9iodns9ihlh2q0222.apps.googleusercontent.com',
+  googleClientSecret: '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',
   mongoURI:
-    "mongodb+srv://pini85:Q16DdbVyAiQgeG83@cluster0.mf3qt.mongodb.net/find-my-movie?retryWrites=true&w=majority",
-  cookieKey: "tfgwe4y634y754uyh56jh46u6ur6tgfy6hgu67h",
+    'mongodb+srv://pini85:Q16DdbVyAiQgeG83@cluster0.mf3qt.mongodb.net/find-my-movie?retryWrites=true&w=majority',
+  cookieKey: 'tfgwe4y634y754uyh56jh46u6ur6tgfy6hgu67h',
 };
